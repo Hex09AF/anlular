240ec2b9bd5352b7895dbc41539b0bcb9e6687f5
Hex09AF <lsqhuy@gmail.com>
2023-08-02 22:12:01 +0700

    feat: total

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,20 +2,20 @@ import { Component } from '@angular/core';
 import { InComponent } from '../in/in.component';
 import { OutComponent } from '../out/out.component';
 import { NgIf } from '@angular/common';
+import { TotalComponent } from '../total/total.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [InComponent, OutComponent, NgIf],
+  imports: [InComponent, OutComponent, TotalComponent, NgIf],
 })
 export class HomeComponent {
-  viewIncome = false;
-  viewOutcome = false;
+  viewIncome = true;
+  viewOutcome = true;
 
   toggleView(type: string) {
-    console.log(type);
     switch (type) {
       case 'INCOME':
         this.viewIncome = !this.viewIncome;
---
+++ src/app/total/total.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-total',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './total.component.html',
+  styleUrls: ['./total.component.scss'],
+})
+export class TotalComponent {}
